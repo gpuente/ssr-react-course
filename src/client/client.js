b5527613639b90f12b3ad1458df00c8f04e4828b
Guillermo Puente <gpuente@grouponlatam.com>
2018-01-04 22:28:38 -0300

    added redux to client

--- src/client/client.js
+++ src/client/client.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReaactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
 import Routes from './Routes';
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 const app = document.getElementById('root');
 
 ReaactDOM.hydrate(
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>,
+  <Provider store={store} >
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </Provider>,
   app
 );
